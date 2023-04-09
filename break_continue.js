18415da6849d823d7023979ab10e337c90d8562e
RamAsheesh Chauhan <rch231198@gmail.com>
2023-04-09 13:29:21 +0530

    upToDate

---
+++ break_continue.js
@@ -0,0 +1,19 @@
+// let a= 10
+// while(a<100){
+//     console.log(a)
+//     a+=10
+//     if(a==50){
+//         break
+//     }
+// }
+
+// continue
+
+let a= 10
+while(a<100){
+    a+=10
+    if(a==30 || a ==50 || a==70){
+        continue
+    }
+    console.log(a)
+}
---
+++ flexible_if_block.js
@@ -0,0 +1,9 @@
+//In 'if block' we can also pass a (integer, string etc) value other boolean
+
+let val = [10,0,-99,"string","",null,undefined]
+
+if(val[4]){
+    console.log("This is true")
+} else {
+    console.log("This is false")
+}

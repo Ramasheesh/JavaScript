5d4fbe92af5d9f0b90858bfb9e5c085de232cbec
RamAsheesh Chauhan <rch231198@gmail.com>
2023-04-10 22:10:06 +0530

    all done

--- Array/insertArray.js
+++ Array/insertArray.js
@@ -1,15 +1,13 @@
 let data = [10,2,55,65,24,21]
 let neEl = 100;
 let pos = 2;
-for(let i = data.length;i>=0 ;i--){
+for(let i = 0 ; i<=data.length-1 ;i++){
     if(i==pos){
-        data.length = data.length+1
+        data[data.length] = data[pos]
+        data[pos] = neEl;
 
-        data[i+1] = data[i]
-        data.length[i] = data.length+1
-        data[pos]= neEl;
-        console.log(data)
     }
 }
 // data.length = data.length+1
-console.log(data);
+console.log("inserted Data-> "+data);
+

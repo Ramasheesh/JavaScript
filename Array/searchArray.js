fcad014e753ee1885b2758a66cffe82c830dbceb
RamAsheesh Chauhan <rch231198@gmail.com>
2023-04-10 22:13:05 +0530

    Update searchArray.js

--- Array/searchArray.js
+++ Array/searchArray.js
@@ -1,11 +1,9 @@
 let data = [40,62,94,05,66,87,5,80]
 let itemSrch = 66;
 for(let  i = 0 ; i<data.length-1;i++){
-    // var index = ""
     if(data[i] === itemSrch){
         console.log(i)
-        // index=i
-        // console.log(index)
+        console.log(data[i])
         break
     }
 } 

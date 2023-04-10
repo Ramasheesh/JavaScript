a98beb9b63c575028dcc693dfdb587a556b44470
RamAsheesh Chauhan <rch231198@gmail.com>
2023-04-10 10:04:13 +0530

    Updated

---
+++ String/ReplaceString.js
@@ -0,0 +1,2 @@
+let str = " W3 school"
+console.log(str.replace("school","'this is replace new string'"))
---
+++ String/stringSearch.js
@@ -0,0 +1,2 @@
+let str = "hi man ! can u help me out find this man"
+console.log(str.search("man"))

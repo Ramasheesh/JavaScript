0e37a94dc0e84b817288c2fdd4e54effebb072d4
RamAsheesh Chauhan <rch231198@gmail.com>
2023-04-12 14:11:54 +0530

    change updated

---
+++ Array/array_in_js.js
@@ -0,0 +1,28 @@
+/*
+Create an Array
+Add Elements to Existing Array
+Retrieve Elements from Array
+Concept of Length
+*/
+
+let a = [10,20,30,40,50]
+
+let b = ["Sun","Mon","Tue","Wed"]
+
+a[0] = 100
+b[1] = "Sat"
+a[10] = 60
+console.log(a[3])
+console.log(b[5])
+
+console.log(a.length)
+
+let b = {
+    0 : 6,
+    1 : 12,
+    2 : 24,
+    3 : 48,
+    4 : 96,
+    5 : 192,
+    length : 6
+}
---
+++ Array/searching_in_array.js
@@ -0,0 +1,22 @@
+/*
+Search items in Array - INCLUDES
+Find first index of an item - INDEXOF
+Find last index of an item - LASTINDEXOF
+*/
+
+const planets = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune",
+  "Venus"
+];
+
+console.log(planets.includes("Earth"))
+console.log(planets.indexOf("Venus"))
+console.log(planets.lastIndexOf("Venus"))
+
